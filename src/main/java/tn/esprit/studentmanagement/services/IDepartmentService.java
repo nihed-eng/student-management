b7eb1f69f38607b133ecc6c07cdf2f987ad9782f
Nihed Mathlouthi <Nihed.Mathlouthi@esprit.tn>
2026-02-24 11:50:36 +0100

    Update IDepartmentService.java

--- src/main/java/tn/esprit/studentmanagement/services/IDepartmentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/IDepartmentService.java
@@ -1,38 +1,15 @@
 package tn.esprit.studentmanagement.services;
 
-import org.springframework.stereotype.Service;
 import tn.esprit.studentmanagement.entities.Department;
-import tn.esprit.studentmanagement.repositories.DepartmentRepository;
-
 import java.util.List;
 
-@Service
-public class DepartmentService implements IDepartmentService {
+public interface IDepartmentService {
 
-    private final DepartmentRepository departmentRepository;
+    List<Department> getAllDepartments();
 
-    public DepartmentService(DepartmentRepository departmentRepository) {
-        this.departmentRepository = departmentRepository;
-    }
+    Department getDepartmentById(Long idDepartment);
 
-    @Override
-    public List<Department> getAllDepartments() {
-        return departmentRepository.findAll();
-    }
+    Department saveDepartment(Department department);
 
-    @Override
-    public Department getDepartmentById(Long idDepartment) {
-        return departmentRepository.findById(idDepartment)
-                .orElseThrow(() -> new RuntimeException("Department not found"));
-    }
-
-    @Override
-    public Department saveDepartment(Department department) {
-        return departmentRepository.save(department);
-    }
-
-    @Override
-    public void deleteDepartment(Long idDepartment) {
-        departmentRepository.deleteById(idDepartment);
-    }
+    void deleteDepartment(Long idDepartment);
 }
