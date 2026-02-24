0bc662d95b823056321cbc4512544fd6d2a60310
Nihed Mathlouthi <Nihed.Mathlouthi@esprit.tn>
2026-02-24 11:17:54 +0100

    Clean up Student class by removing unused code
    Removed unused import and ToString annotation.

--- src/main/java/tn/esprit/studentmanagement/entities/Student.java
+++ src/main/java/tn/esprit/studentmanagement/entities/Student.java
@@ -2,7 +2,6 @@ package tn.esprit.studentmanagement.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,11 +11,12 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idStudent;
+
     private String firstName;
     private String lastName;
     private String email;
