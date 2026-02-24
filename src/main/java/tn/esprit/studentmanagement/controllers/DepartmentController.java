57d7e1e43feee4d1572f1773482b46e9188e5e02
Nihed Mathlouthi <Nihed.Mathlouthi@esprit.tn>
2026-02-24 11:15:32 +0100

    Fix request mapping and refactor methods in DepartmentController
    Refactor DepartmentController for improved readability and structure.

--- src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
+++ src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
@@ -1,29 +1,36 @@
 package tn.esprit.studentmanagement.controllers;
 
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.studentmanagement.entities.Department;
-import tn.esprit.studentmanagement.entities.Enrollment;
-import tn.esprit.studentmanagement.services.DepartmentService;
 import tn.esprit.studentmanagement.services.IDepartmentService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/Depatment")
+@RequestMapping("/Department")
 @CrossOrigin(origins = "http://localhost:4200")
-@AllArgsConstructor
 public class DepartmentController {
-    private IDepartmentService departmentService;
+
+    private final IDepartmentService departmentService;
+
+    public DepartmentController(IDepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
 
     @GetMapping("/getAllDepartment")
-    public List<Department> getAllDepartment() { return departmentService.getAllDepartments(); }
+    public List<Department> getAllDepartment() {
+        return departmentService.getAllDepartments();
+    }
 
     @GetMapping("/getDepartment/{id}")
-    public Department getDepartment(@PathVariable Long id) { return departmentService.getDepartmentById(id); }
+    public Department getDepartment(@PathVariable Long id) {
+        return departmentService.getDepartmentById(id);
+    }
 
     @PostMapping("/createDepartment")
-    public Department createDepartment(@RequestBody Department department) { return departmentService.saveDepartment(department); }
+    public Department createDepartment(@RequestBody Department department) {
+        return departmentService.saveDepartment(department);
+    }
 
     @PutMapping("/updateDepartment")
     public Department updateDepartment(@RequestBody Department department) {
@@ -32,5 +39,6 @@ public class DepartmentController {
 
     @DeleteMapping("/deleteDepartment/{id}")
     public void deleteDepartment(@PathVariable Long id) {
-      departmentService.deleteDepartment(id); }
+        departmentService.deleteDepartment(id);
+    }
 }
